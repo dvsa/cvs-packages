f264a122b2d1abb495c5b8f72142ab8349a1da1f
Matthew Bell <mbell.dvsa@icloud.com>
2024-02-08 11:59:43 +0000

    Add SES client; Add unit testing for AWS classes;

---
+++ src/classes/aws/__tests__/secrets-manager-client.spec.ts
@@ -0,0 +1,57 @@
+import { SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
+import { SecretsManager } from '../secrets-manager-client';
+
+jest.mock('@aws-sdk/credential-providers', () => ({
+  fromIni: jest.fn().mockReturnValue('iniCredentials'),
+}));
+
+jest.mock('@aws-sdk/client-secrets-manager', () => {
+  const originalModule = jest.requireActual('@aws-sdk/client-secrets-manager');
+
+  // Mock the SecretsManagerClient class
+  return {
+    ...originalModule,
+    SecretsManagerClient: jest.fn().mockImplementation(() => ({
+      send: jest.fn().mockImplementation((command) => {
+        if (command.input.SecretId === 'validSecretId') {
+          return Promise.resolve({
+            SecretString: JSON.stringify({ secretKey: 'secretValue' }),
+          });
+        } else if (command.input.SecretId === 'emptySecret') {
+          return Promise.resolve({
+            SecretString: JSON.stringify({}),
+          });
+        } else {
+          return Promise.reject(new Error('Secret not found'));
+        }
+      }),
+    })),
+  };
+});
+
+describe('SecretsManager', () => {
+  describe('get', () => {
+    it('should use credentials from ini file when USE_CREDENTIALS is true', () => {
+      process.env.USE_CREDENTIALS = 'true';
+      SecretsManager.getClient({});
+      expect(SecretsManagerClient).toHaveBeenCalledWith(
+        expect.objectContaining({
+          credentials: 'iniCredentials',
+        })
+      );
+    });
+
+    it('returns a parsed secret when given a valid secret ID', async () => {
+      const secret = await SecretsManager.get({ SecretId: 'validSecretId' });
+      expect(secret).toEqual({ secretKey: 'secretValue' });
+    });
+
+    it('throws an error when the secret ID is invalid', async () => {
+      await expect(SecretsManager.get({ SecretId: 'invalidSecretId' })).rejects.toThrow('Secret not found');
+    });
+
+    it('throws an error when the secret string is empty', async () => {
+      await expect(SecretsManager.get({ SecretId: 'emptySecret' })).rejects.toThrow('Secret string \'emptySecret\' was empty.');
+    });
+  });
+});
---
+++ src/classes/aws/ses-client.ts
@@ -0,0 +1,80 @@
+import {
+  SESClient,
+  SendEmailCommand,
+  SESClientConfig,
+  SendEmailCommandOutput,
+} from '@aws-sdk/client-ses';
+import { fromIni } from '@aws-sdk/credential-providers';
+
+export interface SendPayload {
+  to: string[];
+  from: string;
+  subject: string;
+  htmlBody: string;
+  textBody: string;
+  /**
+   * Carbon Copy
+   */
+  cc?: string[];
+  /**
+   * Blind carbon Copy
+   */
+  bcc?: string[];
+}
+
+export class SimpleEmailService {
+
+  /**
+   * Get an SES client
+   * - If process.env.USE_CREDENTIALS is true, credentials will be used from ~/.aws/credentials
+   * @param {Partial<SESClientConfig>} config - The SES configuration object.
+   * @returns {SESClient}
+   */
+  static getClient(
+    config: Partial<SESClientConfig> = {},
+  ): SESClient {
+    if (process.env.USE_CREDENTIALS === 'true') {
+      config.credentials = fromIni();
+    }
+
+    return new SESClient(config);
+  }
+
+  /**
+   * Send an email using AWS SES, using a pre-defined template
+   * @param {SendPayload} payload - The parameters to send to the operation.
+   * @param {Partial<SESClientConfig>} config - The SES configuration object.
+   * @returns {Promise<SendEmailCommandOutput>}
+   */
+  static async send(
+    payload: SendPayload,
+    config: Partial<SESClientConfig> = {},
+  ): Promise<SendEmailCommandOutput> {
+    return SimpleEmailService.getClient(config).send(
+      new SendEmailCommand({
+        Source: payload.from,
+        Destination: {
+          ToAddresses: payload.to,
+          BccAddresses: payload.bcc || [],
+          CcAddresses: payload.cc || [],
+        },
+        Message: {
+          Subject: {
+            Charset: 'UTF-8',
+            Data: payload.subject,
+          },
+          Body: {
+            Html: {
+              Charset: 'UTF-8',
+              Data: `<h1>${payload.htmlBody}</h1>`,
+            },
+            Text: {
+              Charset: 'UTF-8',
+              Data: payload.textBody,
+            },
+          },
+        },
+      }),
+    );
+  }
+}
