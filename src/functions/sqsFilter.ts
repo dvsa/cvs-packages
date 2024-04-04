21c6b89d7813d07df259d9d61963654302102fad
naathanbrown <nathancjb@googlemail.com>
2024-04-04 10:36:38 +0100

    feat(cb2-10603): make filter generic

--- src/functions/sqsFilter.ts
+++ src/functions/sqsFilter.ts
@@ -1,9 +1,8 @@
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const processRecord = (record: any) => {
-  const recordBody = JSON.parse(JSON.parse(record.body).Message);
-  if (recordBody.eventName === 'MODIFY' && recordBody.dynamodb && recordBody.dynamodb.NewImage) {
+export const processRecord = (recordBody: any, eventName: string = 'MODIFY') => {
+  if (recordBody.eventName === eventName && recordBody.dynamodb && recordBody.dynamodb.NewImage) {
     return unmarshall(recordBody.dynamodb.NewImage);
   }
   console.warn(`process filtered to undefined, record is ${JSON.stringify(recordBody)}`);
