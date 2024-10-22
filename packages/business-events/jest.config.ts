96627dafc907664c71fa4d66c61acb0438813043
Matt Waldron <matt.waldron@bjss.com>
2024-10-22 12:37:31 +0100

    The initial version of the business events package

---
+++ packages/business-events/jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+};
+
+export default config;
