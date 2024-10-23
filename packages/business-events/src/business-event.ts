b92178dce0095a527547849779a4b27700c7ff44
Matt Waldron <matt.waldron@bjss.com>
2024-10-23 10:13:12 +0100

    pr feedback - introduce biome and lint

--- packages/business-events/src/business-event.ts
+++ packages/business-events/src/business-event.ts
@@ -4,28 +4,28 @@ import { PublishCommand } from '@aws-sdk/client-sns';
  * A simple decorator for deserialising the publish command.
  */
 export class BusinessEvent {
-  public command: PublishCommand;
+	public command: PublishCommand;
 
-  /**
-   * Creates a new business event with an initialised SNS publish command.
-   * @param topicArn The topic ARN the event will be published to.
-   * @param event The event name being published.
-   * @param subType The event subtype being published.
-   * @param contextAttributes Additional attributes relevant to the business event.
-   */
-  constructor(
-    public topicArn: string,
-    public event: string,
-    public subType?: string,
-    public contextAttributes?: Record<string, string>
-  ) {
-    this.command = new PublishCommand({
-      TopicArn: this.topicArn,
-      Message: JSON.stringify({
-        event: this.event,
-        subType: this.subType,
-        contextAttributes: this.contextAttributes,
-      }),
-    });
-  }
+	/**
+	 * Creates a new business event with an initialised SNS publish command.
+	 * @param topicArn The topic ARN the event will be published to.
+	 * @param event The event name being published.
+	 * @param subType The event subtype being published.
+	 * @param contextAttributes Additional attributes relevant to the business event.
+	 */
+	constructor(
+		public topicArn: string,
+		public event: string,
+		public subType?: string,
+		public contextAttributes?: Record<string, string>
+	) {
+		this.command = new PublishCommand({
+			TopicArn: this.topicArn,
+			Message: JSON.stringify({
+				event: this.event,
+				subType: this.subType,
+				contextAttributes: this.contextAttributes,
+			}),
+		});
+	}
 }
--- packages/business-events/src/event-publisher.ts
+++ packages/business-events/src/event-publisher.ts
@@ -8,48 +8,44 @@ import { BusinessEventValidator } from './business-event-validator';
  * Publishes business events for a software system.
  */
 export class BusinessEventPublisher {
-  private static readonly defaultConfig: Partial<SNSClientConfig> = {
-    region: 'eu-west-1',
-  };
+	private static readonly defaultConfig: Partial<SNSClientConfig> = {
+		region: 'eu-west-1',
+	};
 
-  /**
-   * Get an SNS client
-   * - If process.env.USE_CREDENTIALS is true, credentials will be used from ~/.aws/credentials
-   * @param {Partial<SNSClientConfig>} config - The SNS configuration object.
-   * @returns {SNSClient}
-   */
-  static getClient(
-    config: Partial<SNSClientConfig> = BusinessEventPublisher.defaultConfig
-  ): SNSClient {
-    if (process.env.USE_CREDENTIALS === 'true') {
-      config.credentials = fromIni();
-    }
+	/**
+	 * Get an SNS client
+	 * - If process.env.USE_CREDENTIALS is true, credentials will be used from ~/.aws/credentials
+	 * @param {Partial<SNSClientConfig>} config - The SNS configuration object.
+	 * @returns {SNSClient}
+	 */
+	static getClient(config: Partial<SNSClientConfig> = BusinessEventPublisher.defaultConfig): SNSClient {
+		if (process.env.USE_CREDENTIALS === 'true') {
+			config.credentials = fromIni();
+		}
 
-    // If tracing is enabled, then capture the client with AWS X-Ray
-    return process.env._X_AMZN_TRACE_ID
-      ? captureAWSv3Client(new SNSClient(config))
-      : new SNSClient(config);
-  }
+		// If tracing is enabled, then capture the client with AWS X-Ray
+		return process.env._X_AMZN_TRACE_ID ? captureAWSv3Client(new SNSClient(config)) : new SNSClient(config);
+	}
 
-  /**
-   * Publishes a business event to an SNS topic.
-   * @param payload The event to publish.
-   * @param config The SNS client configuration to use for publishing.
-   */
-  static async publish(
-    payload: BusinessEvent,
-    config: Partial<SNSClientConfig> = BusinessEventPublisher.defaultConfig
-  ) {
-    try {
-      const validator = new BusinessEventValidator(payload);
-      validator.validate();
-      if (validator.error) {
-        throw new Error(validator.message);
-      }
+	/**
+	 * Publishes a business event to an SNS topic.
+	 * @param payload The event to publish.
+	 * @param config The SNS client configuration to use for publishing.
+	 */
+	static async publish(
+		payload: BusinessEvent,
+		config: Partial<SNSClientConfig> = BusinessEventPublisher.defaultConfig
+	) {
+		try {
+			const validator = new BusinessEventValidator(payload);
+			validator.validate();
+			if (validator.error) {
+				throw new Error(validator.message);
+			}
 
-      return this.getClient(config).send(payload.command);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+			return BusinessEventPublisher.getClient(config).send(payload.command);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 }
